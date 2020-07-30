c593d6b11b9a0abff9955c8f754002549c8d2057
PlayerU99 <67335788+PlayerU99@users.noreply.github.com>
2020-07-31 03:31:22 +0530

    My First Page

---
+++ Browser-sync.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\arpan\My-Coursera\site
+browser-sync --version
+browser-sync start --server --directory --files "*"
---
+++ Update_HTML_Github.bat
@@ -0,0 +1,5 @@
+cd /d C:\Users\arpan\My-Coursera
+git --version
+git add .
+git commit -m "My First Page"
+git push
