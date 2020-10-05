4595f46a1b2f387797fb15239683b00e10cba372
PlayerU99 <67335788+PlayerU99@users.noreply.github.com>
2020-10-05 22:45:28 +0530

    My Project Update

---
+++ Browser-sync for Mod2.bat
@@ -0,0 +1,2 @@
+cd "C:\My-Coursera\Module_2_Assignment"
+browser-sync start --server --directory --files "**/*"
--- Browser-sync.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\My-Coursera"
-browser-sync --version
-browser-sync start --server --directory --files "*"
